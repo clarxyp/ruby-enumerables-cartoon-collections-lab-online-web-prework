8146e05452c6b59b658e5cebdf60111e16bdad0e
Clara Yip <clara.yip1@gmail.com>
2019-07-05 23:44:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map { |call| #{call.capitalize} + "!" 
+  array.map { |call| 
+  puts #{call.capitalize} + "!" 
   } 
 end
 
