b9429f6169cd707aad6868ccabd2c4a9b5ba3da7
Clara Yip <clara.yip1@gmail.com>
2019-07-06 00:27:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find { |i|
-    if cheese_types.include? (x)
-      
+    if cheese_types.include? (i)
+      return i
 end
